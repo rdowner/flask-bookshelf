19ce2286d33dcc82b230a28e2e806cae3d5e9dd2
Richard Downer <richard@apache.org>
2018-06-20 12:09:10 +0100

    Update cfn template and export script

--- export.sh
+++ export.sh
@@ -5,5 +5,10 @@ set -e
 MYTMPDIR="$(mktemp -d)"
 trap 'rm -rf "${MYTMPDIR}"' EXIT
 
+COMMIT=HEAD
+
+COMMITID="$( git rev-parse --short $COMMIT )"
+TIMESTAMP="$( git show -s --format=%cd --date=format:'%Y%m%d-%H%M%S' $COMMIT )"
+
 git -c core.autocrlf=false -c core.eol=lf archive HEAD | gzip > "${MYTMPDIR}/flask-bookshelf.tar.gz"
-aws --profile downer-scratch s3 cp "${MYTMPDIR}/flask-bookshelf.tar.gz" s3://downer-codedeploy
+aws --profile downer-scratch s3 cp "${MYTMPDIR}/flask-bookshelf.tar.gz" s3://downer-codedeploy/flask-bookshelf-${TIMESTAMP}-${COMMITID}.tar.gz
